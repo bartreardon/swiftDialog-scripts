83825ef4640e51bb6f3419b2304b1c03c8b93484
Bart Reardon <bart.reardon@csiro.au>
2022-11-11 09:16:21 +1100

    Update get_json_value.sh
    made the comment an editor field
    fixed a typo that prevented `department` value from being printed

--- JSON/get_json_value.sh
+++ JSON/get_json_value.sh
@@ -3,8 +3,8 @@
 # This function can be used to parse JSON results from a dialog command
 
 function get_json_value () {
-	# usage: get_json_value "$JSON" "key 1" "key 2" "key 3"	 
-	for var in "${@:2}"; do jsonkey="${jsonkey}['${var}']"; done
+    # usage: get_json_value "$JSON" "key 1" "key 2" "key 3"     
+    for var in "${@:2}"; do jsonkey="${jsonkey}['${var}']"; done
     JSON="$1" osascript -l 'JavaScript' \
         -e 'const env = $.NSProcessInfo.processInfo.environment.objectForKey("JSON").js' \
         -e "JSON.parse(env)$jsonkey"
@@ -16,39 +16,42 @@ UserPromptJSON='{
   "title" : "Device Setup",
   "message" : "Please set a computer name and choose the appropriate State and Department for where you normally work.\n\nFeel free to also leave a comment",
   "textfield" : [
-  	 {"title" : "Computer Name", "required" : false, "prompt" : "Computer Name"},
-  	 {"title" : "Comment", "required" : false, "prompt" : "Enter a comment"}
+       {"title" : "Computer Name", "required" : false, "prompt" : "Computer Name"},
+       {"title" : "Comment", "required" : false, "prompt" : "Enter a comment", "editor" : true}
   ],
   "selectitems" : [
     {"title" : "Select State", "values" : ["ACT","NSW","VIC","QLD","TAS","SA","WA","NT"]},
     {"title" : "Department",
     "values" : [
-			  "Business Development",
-			  "Chief of Staff",
-		  	"Commercial",
-			  "Corporate Affairs",
-			  "Executive",
-		  	"Finance",
-		  	"Governance",
-			  "Human Resources",
-		  	"Information Technology",
-		  	"Services"
-			]
-	}],
-	"icon" : "SF=info.circle",
-	"centreicon" : true,
-	"alignment" : "centre",
-	"button1text" : "Next",
-	"height" : "450"
+              "Business Development",
+              "Chief of Staff",
+              "Commercial",
+              "Corporate Affairs",
+              "Executive",
+              "Finance",
+              "Governance",
+              "Human Resources",
+              "Information Technology",
+              "Services"
+            ]
+    }],
+    "icon" : "SF=info.circle",
+    "centreicon" : true,
+    "alignment" : "centre",
+    "button1text" : "Next",
+    "height" : "450"
 }'
 
 # make a temp file for storing our JSON
 tempfile=$(mktemp)
 echo $UserPromptJSON > $tempfile
 
-# run dialog and store the JSON results in a variable
-results=$(/usr/local/bin/dialog --jsonfile $tempfile --json)
+dialogcmd="/Users/rea094/Library/Developer/Xcode/DerivedData/dialog-anicpqacfliuhtfvaslmyvexkwkn/Build/Products/Debug/Dialog.app/Contents/MacOS/Dialog"
 
+# run dialog and store the JSON results in a variable
+#${dialogcmd} --jsonfile $tempfile --json
+#exit
+results=$(${dialogcmd} --jsonfile $tempfile --json)
 # clean up
 rm $tempfile
 
@@ -59,7 +62,7 @@ compname=$(get_json_value "$results" "Computer Name")
 comment=$(get_json_value "$results" "Comment")
 
 echo "Computer name is $compname"
-echo "Department is $division"
+echo "Department is $department"
 echo "State is $state"
 echo "Comment is $comment"
 
