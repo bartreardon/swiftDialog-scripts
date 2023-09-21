4495aa5b8c9437977a66059548ae914812a9c6c5
Bart Reardon <bart.reardon@csiro.au>
2023-09-21 15:45:40 +1000

    Create reportVersionForLabel.sh

---
+++ Installomator/reportVersionForLabel.sh
@@ -0,0 +1,107 @@
+#!/bin/zsh
+
+#
+# Process the list of labels, download directly from the Installomator git repo
+# process the labels and report the version number
+# Will also record the last run in a file and compare against the last known version
+# If there is a new version, a report is generated with the updated labels and what version is the latest
+# 
+
+
+# labels we want to process
+labels=(
+    "microsoftedge"
+    "firefoxpkg" 
+    "macadminspython"
+    "microsoftvisualstudiocode"
+)
+
+RAWInstallomatorURL="https://raw.githubusercontent.com/Installomator/Installomator/main"
+appListFile="applist.txt"
+
+# backup existing file
+if [[ -e ${appListFile} ]]; then
+    cp "${appListFile}" "${appListFile}-$(date '+%Y-%d-%m')"
+else
+    touch "${appListFile}"
+fi
+
+# load functions from Installomator
+functionsPath="/var/tmp/functions.sh"
+curl -sL ${RAWInstallomatorURL}/fragments/functions.sh -o "${functionsPath}"
+source "${functionsPath}"
+
+# additional functions
+labelFromInstallomator() {
+    echo "${RAWInstallomatorURL}/fragments/labels/$1.sh"
+}
+
+checkExistingVersion() {
+    key=$1
+    value=$2
+    previousVersion=$(grep "${key}"  ${appListFile} | awk '{print $NF}')
+
+    if [[ "$previousVersion" != "$value" ]]; then
+        if [[ -n $previousVersion ]]; then 
+            echo "New App $key -> $value"
+            # app not found - add to the  appListFile 
+            echo "$key $value" >>  ${appListFile}
+        else
+            echo "Updating $key from $previousVersion -> $value"
+            # update the  appListFile 
+            sed -i "" "s/^$key .*/$key $value/g" ${appListFile}
+        fi
+        formattedOutput+="$key $value, "
+    else
+        echo "No Update for $key -> $value"
+    fi
+}
+
+# process each label
+for label in $labels; do
+    echo "Processing label $label"
+
+    # get label fragment from Installomator repo
+    fragment=$(curl -sL $(labelFromInstallomator $label))
+    if [[ "$fragment" == *"404"* ]]; then
+        echo "no fragment for label $label"
+        continue
+    fi
+    
+    # trim the first lines and anything from ;; onward since we want to eval the label, not use it in a switch statement
+    # this assumes the first line after the pattern is `name=`
+    fragment=$(echo "$fragment" | sed -n '/name=/,$p' | sed -e '/;;/,$d')
+
+    eval $fragment
+    
+    if [[ -n $name ]]; then
+        previousVersion=$(grep -e "^${name} " ${appListFile} | awk '{print $NF}')
+        # read -s -k '?Press any key to continue.'
+        if [[ "$previousVersion" != "$appNewVersion" ]]; then
+            if [[ -z $previousVersion ]]; then 
+                echo "New App $name -> $appNewVersion"
+                # app not found - add to the  appListFile 
+                echo "$name $appNewVersion" >> ${appListFile}
+            else
+                echo "Updating $name from $previousVersion -> $appNewVersion"
+                # update the  appListFile 
+                sed -i "" "s/^$name .*/$name $appNewVersion/g"  ${appListFile}
+            fi
+            formattedOutput+="$name $appNewVersion, "
+        else
+            echo "No Update for $name -> $appNewVersion"
+        fi
+    fi
+    unset appNewVersion
+    unset name
+    unset previousVersion
+done
+
+echo "**** text for report"
+echo ""
+echo $formattedOutput
+echo ""
+echo "****"
+
+# clean up 
+rm "$functionsPath"
